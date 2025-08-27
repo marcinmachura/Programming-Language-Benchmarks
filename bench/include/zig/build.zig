90301ce83b2328e04bd3aeccbc6a6a6177b79fc7
Marcin Machura <marcin.machura@hotmail.com>
2025-08-27 12:40:18 +0100

    adding releaseFast build param

--- bench/include/zig/build.zig
+++ bench/include/zig/build.zig
@@ -7,16 +7,16 @@ pub fn build(b: *std.Build) void {
     // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
     // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we default
+    // to ReleaseFast for benchmark performance.
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
     const exe = b.addExecutable(.{
         .name = "app",
         .root_module = b.createModule(.{ .root_source_file = b.path("app.zig") }),
     });
     exe.setTarget(target);
-    exe.setBuildMode(optimize);
+    exe.setOptimizeMode(optimize);
     exe.linkLibC();
     b.installArtifact(exe);
 
